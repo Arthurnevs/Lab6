8f700a23246cc5f8ef65390d6b60368ade326335
Arthurnevs <55116700+Arthurnevs@users.noreply.github.com>
2020-11-25 22:18:21 -0300

    Add files via upload

---
+++ Lab7/test/ufcg/ccc/domino/JogadaTest.java
@@ -0,0 +1,20 @@
+package ufcg.ccc.domino;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JogadaTest {
+
+	@Test
+	void testJogadaPassa() {
+		Jogada j1 = new Jogada();
+		assertEquals(TipoJogada.PASSA, j1.getTipo());
+	}
+
+	@Test
+	void testJogadaComPeca() {
+		Jogada j1 = new Jogada(new Peca(1, 1), TipoJogada.NA_ESQUERDA);
+		assertEquals(TipoJogada.NA_ESQUERDA, j1.getTipo());
+	}
+}
